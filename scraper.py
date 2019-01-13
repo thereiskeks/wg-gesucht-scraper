84fc89e8cfbbd0bb83b6bf7de7a403f0a2b95697
felix baral <felixbaral@me.com>
2019-01-13 13:57:02 +0100

    made it fitting to my usecase

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 import requests
+from lxml.etree import _Attrib
 from pyquery import PyQuery as pq
 from collections import namedtuple
+import sys
+
+parameters = sys.argv
 
 class Offer(object):
     pass
@@ -12,10 +16,23 @@ try:
 except:
     pass
 
-resp = requests.get('http://www.wg-gesucht.de/en/wohnungen-in-Berlin.8.2.0.0.html?filter=97d0cd356b0f749d3524d653301d91be410257280986c8c4ba')
+def bot_sendtext(bot_message):
+    ### Send text message
+    print("sending")
+    bot_token = parameters[1]
+    bot_chatID = parameters[2]
+    send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id=' + bot_chatID + '&text=' + bot_message
+    print(send_text)
+    requests.get(send_text)
+
+
+resp = requests.get('https://www.wg-gesucht.de/wg-zimmer-in-Muenchen.90.0.1.0.html#back_to_ad_3689803')
 d = pq(resp.text)
-trs = d('#table-compact-list tr').not_('.inlistTeaser')[2:]
-ids = [tr.attrib['adid'] for tr in trs]
+trs:_Attrib = d('#main_column div.panel')
+ids = [tr.attrib['data-id'] for tr in trs if tr.attrib.has_key("data-id")]
+print("-")
+
+
 
 try:
     lastpos = ids.index(lastid)
@@ -27,11 +44,22 @@ else:
     trs = trs[:lastpos]
     ids = ids[:lastpos]
 
-def parse_tr(tr):
-    tds = [td.text_content().strip() for td in e.findall('td')][2:]
-    tds.append('http://wg-gesucht.de/' + tr.attrib['adid'])
-    return tds
+def generateLink(id):
+    return 'http://wg-gesucht.de/' + id + ".html"
 
-data = [parse_tr(e) for e in trs]
-print '\n'.join('\t'.join(e) for e in data).encode('utf-8')
+data = [generateLink(e) for e in ids]
+print('\n'.join('\t'.join(e) for e in data).encode('utf-8'))
 open('lastid.tmp', 'w').write(ids[0])
+
+
+import datetime
+import time
+st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d %H:%M:%S')
+bot_sendtext("I, your android of choice, found new Flats for you: \n\r"+st)
+
+for link in data:
+    bot_sendtext(link)
+
+
+
+
