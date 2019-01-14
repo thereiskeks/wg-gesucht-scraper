0871ad58cfb5f6cb198d09f509b76529ef16d8cd
felix baral <felixbaral@me.com>
2019-01-14 23:54:31 +0100

    prohibits reposts

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import pickle
+
 import requests
 from lxml.etree import _Attrib
 from pyquery import PyQuery as pq
@@ -10,14 +12,34 @@ class Offer(object):
     pass
 
 lastid=''
+
+
+def save_obj(obj, name ):
+    with open( name + '.pkl', 'wb+') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open( name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+try:
+    lastIds = load_obj("lastIds")
+except:
+    lastIds = {}
+
 try:
     with open('lastid.tmp', 'r') as f:
         lastid = f.read().strip()
+
+
 except:
     pass
 
 def bot_sendtext(bot_message):
     ### Send text message
+    if len(parameters) < 3:
+        print("you need to povide a bot_token and a chat_id")
+        return
     print("sending")
     bot_token = parameters[1]
     bot_chatID = parameters[2]
@@ -38,19 +60,28 @@ try:
     lastpos = ids.index(lastid)
 except:
     lastpos = len(ids)
+
+
+for id in ids:
+    if id in lastIds:
+        lastIds[id] += 1
+    else:
+        lastIds[id] = 0
+
 if lastpos == 0:
     exit(1)
 else:
-    trs = trs[:lastpos]
     ids = ids[:lastpos]
+
 
 def generateLink(id):
     return 'http://wg-gesucht.de/' + id + ".html"
 
-data = [generateLink(e) for e in ids]
+data = [generateLink(e) for e in ids if lastIds[id] < 5]
 print('\n'.join('\t'.join(e) for e in data).encode('utf-8'))
 open('lastid.tmp', 'w').write(ids[0])
 
+save_obj(lastIds, "lastIds")
 
 import datetime
 import time
